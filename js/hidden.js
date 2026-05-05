a61fe81ab7dfae28fefbf303aa21c86245a018bc
Onik-Howlader <mdtahsanislamonik6@gmail.com.com>
2026-05-06 01:22:44 +0600

    complete html

---
+++ js/hidden.js
@@ -0,0 +1,11 @@
+// function play(){
+//     const homeSection = document.getElementById('homePart');
+//     homeSection.classList.add('hidden') 
+//     const playGround = document.getElementById('play-ground');
+//     playGround.classList.remove('hidden')
+// }
+
+function play(){
+    hideElementById('homePart');
+    showElementById('play-ground');
+}
